34c82abeca1544538a9522be2e6ab225ad12f5c4
BuildTools <unconfigured@null.spigotmc.org>
2020-09-03 23:17:52 +0200

    Dependencies

---
+++ BitFireEngine/BitFire/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <AL/al.h> // Sound
+#include <GLM/glm.hpp> // Quick Maths
+#include <GLEW/glew.h> // OpenGL
+#include <GLFW/glfw3.h> // Window
+
+int main()
+{
+	std::cout << "Yeetus Deletus" << std::endl;
+
+	return 0;
+}
--- BitFireEngine/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cout << "Yeetus Deletus" << std::endl;
-
-	return 0;
-}
